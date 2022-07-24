ced9bfd55aacf26993315268cc021248b65b288d
Ashik Muhammed <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2022-07-24 14:12:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ from utils import temp
 class Bot(Client):
 
     def __init__(self):
-        super().__init__(,
+        super().__init__(
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
